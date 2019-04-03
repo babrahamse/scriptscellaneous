5b00cf90df90201cff6082755474171dcec8380e
Ben Abrahamse <babrahamse@users.noreply.github.com>
2019-04-03 11:25:08 -0400

    Update oclc_apis.py
    added more error trapping
    added 'directory' to output dictionary
    added ability to retrieve duplicate fields by specifying, e.g., '020+' when calling function /get_from_variable

--- oclc_apis.py
+++ oclc_apis.py
@@ -6,16 +6,22 @@ import bs4 as soup
 import requests as rq
 
 def get_oclc_ids_from_search(search_term):
+    search_term = str(search_term) # cast as string
     oclc_ids = []
     submit_URL = ('http://www.worldcat.org/webservices/catalog/search/worldcat/opensearch?q=' +
                   search_term + '&count=100&wskey=' + secrets.OCLC_keys['search'])
     results = rq.get(submit_URL).text
     record = soup.BeautifulSoup(results)
     entries = record.findAll('entry')
-    print('number of results=' + str(len(entries)))
-    for count, entry in enumerate(entries):
-        oclc_ids.append(entry.link['href'].split('/')[-1])
-    return oclc_ids
+    print(entries)
+    if entries == []:
+        print(search_term + ' yielded no results.')
+        return []
+    else:
+        print('number of results=' + str(len(entries)))
+        for count, entry in enumerate(entries):
+            oclc_ids.append(entry.link['href'].split('/')[-1])
+        return oclc_ids
 
 def convert_marcxml_record_to_dict(marcxml_record, dictionary_label):
     # assemble fixed fields entries
@@ -78,7 +84,8 @@ def convert_marcxml_record_to_dict(marcxml_record, dictionary_label):
         variable_fields.update(current_variable_field)
 
     output = {}
-    output.update({'record_'+str(dictionary_label): {'fixed_fields': fixed_fields, 'variable_fields': variable_fields}})
+    output.update({'record_'+str(dictionary_label): {'directory': tag_list, 'fixed_fields': fixed_fields,
+                                                     'variable_fields': variable_fields}})
     return output
 
 def get_record(oclc_id):
@@ -91,27 +98,57 @@ def get_record(oclc_id):
         print('Record retrieved.')
     return record
 
-def get_from_variable(record, fieldtag, sfieldtag, repl_spaces = False, repl_space_char = '/'):
+def get_from_variable(record, fieldtag, sfieldtag):
     output = '' # data in the field/subfield
     multiple_treatment = ('c','f')
     key = list(record)[0]
 
-    try:
+    if len(fieldtag) > 3 and fieldtag[3] == '+':
+        print(fieldtag)
+        duplicate_tags = list(filter(lambda x: fieldtag[0:2] in x, record[key]['variable_fields']))
+        print(duplicate_tags)
+        for current_tag in duplicate_tags:
+            print(current_tag)
+            current_output = ''
+            if sfieldtag == '*':
+                values = list(record[key]['variable_fields'][current_tag])
+                print(values)
+                for sftag in values[1:]:  # skip indicators in position 0
+                    print(sftag)
+                    current_output += ' ' + record[key]['variable_fields'][current_tag][sftag]
+                    current_output = current_output.lstrip()
+                    print(current_output)
+            elif sfieldtag in ['i','I','ind','indicators']:
+                sfieldtag = 'indicators'
+                current_output = record[key]['variable_fields'][current_tag][sfieldtag]
+            else:
+                try:
+                    current_output = record[key]['variable_fields'][current_tag][sfieldtag]
+                except KeyError:
+                    current_output = ('field ' + current_tag + '/subfield ' + sfieldtag + ' not present in record')
+            if output == '':
+                output += current_output
+            else:
+                output += ('|' + current_output)
+    else:
         if sfieldtag == '*':
             values = list(record[key]['variable_fields'][fieldtag])
-            for sftag in values[1:]: # skip indicators in position 0
-                output += (' ' + record[key]['variable_fields'][fieldtag][sftag])
+            print(values)
+            for sftag in values[1:]:  # skip indicators in position 0
+                print(sftag)
+                output += ' ' + record[key]['variable_fields'][fieldtag][sftag]
                 output = output.lstrip()
-        else:
-            if sfieldtag in ['\i','ind','indicators']:
-                sfieldtag = 'indicators'
+                print(output)
+        elif sfieldtag in ['i', 'I', 'ind', 'indicators']:
+            sfieldtag = 'indicators'
             output = record[key]['variable_fields'][fieldtag][sfieldtag]
-    except KeyError:
-        output = 'field not present'
-        return output
+        else:
+            try:
+                current_output = record[key]['variable_fields'][fieldtag][sfieldtag]
+            except KeyError:
+                current_output = (('field ' + fieldtag + '/subfield ' + sfieldtag + ' not present in record'))
 
-    if repl_spaces == True:
-        output = output.replace(' ', repl_space_char)
+    print('final output of function "get_from_variable_field":' + output)
     return output
 
 def record_report(record, report_format = 'c'):
@@ -141,5 +178,14 @@ def get_from_fixed(record, fieldtag, start = 0, length = -1, repl_space = False,
         output = base_string[start:(start + length)]
     except KeyError:
         output = 'field not present'
+        return output
+    if repl_space == True:
+        output = output.replace(' ', repl_space_char)
     return output
+
+
+search_result = get_oclc_ids_from_search('9780974935423')
+record = get_record(search_result[0])
+my_oclc = convert_marcxml_record_to_dict(record, 'test')
+print(my_oclc)
 
